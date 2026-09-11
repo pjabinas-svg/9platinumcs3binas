bf0022389c4c1ecc26f19ede5dddc7d38962867c
pjabinas-svg <pjabinas@brc.pshs.edu.ph>
2026-09-11 10:27:38 +0800

    code

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -8,6 +8,25 @@
         self.__earnings = Earnings
         self.songs = []
 
+    def releaseAlbum(self, title):
+        print(self.name + " released the album " + title)
+
+    def releaseSong(self, title):
+        print(self.name + " released the song " + title)
+
+    def performConcert(self, play):
+        if play:
+            print(self.name + " is performing a concert.")
+        else:
+            print(self.name + " is not performing a concert.")
+
+    def addListeners(self, amount):
+        self.monthly_listeners += amount
+
+    def getEarnings(self, amount):
+        self.__earnings += amount 
+        return self.__earnings
+
     def addSong(self, Song):
         self.songs.append(Song)
 
@@ -23,5 +42,45 @@ class Song:
     def displaySong(self):
         print(self.title + " - " + self.genre)
 
+# Create instances of Artist and Song classes
+Artist1 = Artist(
+    "Opm",
+    "Andalucia",
+    8000000,
+    "IV of Spades",
+    "Philippines, Manila",
+    100000
+)
+Artist2 = Artist(
+    "Jazz",
+    "A matter of Time",
+    30000000,
+    "Laufey",
+    "Iceland, Reykjavik",
+    21000000
+)
 
-    
+# Create instances of Song class
+Song1 = Song("kabiasado", "OPM")
+Song2 = Song("Come in side of my heart", "OPM")
+Song3 = Song("Mundo", "OPM")
+
+print("---before relationship---")
+print(Artist1.name + " has no songs")        
+
+print()
+print("---during relationship---")
+
+Artist1.addSong(Song1)
+Artist1.addSong(Song2)
+Artist1.addSong(Song3)
+
+print("Songs published by " + Artist1.name")
+      
+print()
+print("---after relationship---")
+print("songs related to " + Artist1.name)
+
+  for song in Artist1.songs:
+        song.displaySong()
+ 
